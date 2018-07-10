02556e397e9a56b926f45e314510a1d39f71c0f8
jburto94 <jburto94@yahoo.com>
2018-07-10 16:21:18 -0400

    Added DELETE todos route

--- server/server.js
+++ server/server.js
@@ -51,6 +51,24 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.status(200).send({todo});
+    }).catch(e => {
+      return res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
   console.log(`Server has started at ${port}`);
 });
