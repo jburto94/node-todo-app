7c45b1807330610671b23bacb49e2f8dd25dd09f
jburto94 <jburto94@yahoo.com>
2018-07-10 11:01:04 -0400

    Added post todos route and refactored mongoose

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -31,11 +31,11 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
   //     console.log(result);
   //   });
 
-  db.collection('User')
-    .findOneAndDelete({_id: ObjectID('5b43d6a404a0f325585ed524')})
-    .then(result => {
-      console.log(result);
-    });
+  // db.collection('User')
+  //   .findOneAndDelete({_id: new ObjectID('5b43d6a404a0f325585ed524')})
+  //   .then(result => {
+  //     console.log(result);
+  //   });
   
   //client.close();
 });
